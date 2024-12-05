79c4a9efe6d8b53f1cc0d855690ebc48fc098023
ctpax-nobody <rozmatov.shoxrux08@icloud.com>
2024-12-06 02:37:36 +0500

    save commit

--- handlers/send_music_menu.py
+++ handlers/send_music_menu.py
@@ -1,5 +1,6 @@
 from aiogram.types import Message
 from aiogram import F
+from aiogram.types import ParseMode
 
 from router import router
 from keyboards.reply.music_menu import music_category
@@ -8,3 +9,18 @@ from keyboards.reply.music_menu import music_category
 async def send_categoryes(message: Message):
     await message.answer(text="Kategoriyani tanlang", 
                         reply_markup=music_category())
+
+@router.message(F.text == "Aralash musiqalar 🎵")
+async def send_random_music(message: Message):
+    url = "https://t.me/mymuzzzzzzzzzzzzzzzz"
+    await message.reply(text=f"Guruxga kirish uchun [Muz]({url}) bosing", parse_mode=ParseMode.MARKDOWN)
+
+@router.message(F.text == "Phonklar ☠️")
+async def send_phonk_music(message: Message):
+    url = "https://t.me/anomalyphonk"
+    await message.reply(text=f"Guruxga kirish uchun [Zal time]({url}) bosing", parse_mode=ParseMode.MARKDOWN)
+
+@router.message(F.text == "Peaceful 💆🏻‍♂️")
+async def send_peaceful_music(message: Message):
+    url = "https://t.me/+oNZhavnX3RM4ZjY6"
+    await message.reply(text=f"Guruxga kirish uchun [Peaceful]({url}) bosing", parse_mode=ParseMode.MARKDOWN)
