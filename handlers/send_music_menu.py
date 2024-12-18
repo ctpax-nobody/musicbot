34dfdff55de3d7af0e06c9d1e9f7c7789368fd39
ctpax-nobody <rozmatov.shoxrux08@icloud.com>
2024-12-18 18:05:15 +0500

    save commit

--- handlers/send_music_menu.py
+++ handlers/send_music_menu.py
@@ -3,43 +3,40 @@ from aiogram import F
 from router import router
 from keyboards.reply.send_musics import sending_musics
 from keyboards.reply.music_menu import music_category
+from keyboards.reply.main_menu import main_menu
 
-# Musiqa fayllarini yuborish funksiyasi
 async def send_music_page(message: Message, start_id: int):
     musics_sent = 0
+    skip_count = start_id
 
-    @router.message(F.text == "Musiqalar menyusi 🎧")
-    async def send_categoryes(message: Message):
-        await message.answer(text="Kategoriyani tanlang", 
-                         reply_markup=music_category())
-
-    # Guruhdagi xabarlar tarixini olish
-    async for msg in message.chat.iter_history():
+    async for msg in message.chat.iter_history(reverse=True):
+        if skip_count > 0:
+            skip_count -= 1
+            continue
         if musics_sent >= 10:
-            break  # Faqat 10 ta musiqani yuborish
+            break
         if msg.audio:
             await message.answer_audio(audio=msg.audio.file_id)
             musics_sent += 1
 
-    # Yuborish tugadi, tugmalarni qaytarish
     await message.answer("Musiqalar ro'yxati:", reply_markup=sending_musics())
 
-# "Aralash musiqalar 🎵" tugmasi ishlatilganda
+@router.message(F.text == "Musiqalar menyusi 🎧")
+async def send_categoryes(message: Message):
+    await message.answer("Kategoriyani tanlang:", reply_markup=music_category())
+
 @router.message(F.text == "Aralash musiqalar 🎵")
 async def send_all_musics(message: Message):
     await send_music_page(message, start_id=0)
 
-# Keyingi 10 ta musiqa tugmasi
 @router.message(F.text == "Keyingi 10 ta musiqa 🎵")
 async def send_next_music(message: Message):
     await send_music_page(message, start_id=10)
 
-# Oldingi 10 ta musiqa tugmasi
 @router.message(F.text == "Olidingi 10 ta musiqa 🎵")
 async def send_previous_music(message: Message):
     await send_music_page(message, start_id=0)
 
-# Ortga tugmasi uchun handler
 @router.message(F.text == "Ortga 🔙")
 async def go_back(message: Message):
-    await message.answer("Asosiy menyuga qaytdingiz.")
+    await message.answer("Asosiy menyuga qaytdingiz.", reply_markup=main_menu())
