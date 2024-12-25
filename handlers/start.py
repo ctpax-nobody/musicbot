fe2cbbbbba6f4d3635ae5f28c798903f9089894e
ctpax-nobody <rozmatov.shoxrux08@icloud.com>
2024-12-25 18:13:14 +0500

    save commit

--- handlers/start.py
+++ handlers/start.py
@@ -10,8 +10,18 @@ async def start(message: types.Message):
     telegram_id = message.from_user.id
     fullname = message.from_user.full_name
     username = message.from_user.username
+
     try:
-        if username == True:
+        # Agar bu guruhda bo'lsa
+        if message.chat.type in ['group', 'supergroup']:
+            group_id = message.chat.id
+            await message.reply(
+                text=f"Guruhning ID si: {group_id}",
+                reply_markup=generate_main_menu()
+            )
+
+        # Agar username bo'lsa, uni ro'yxatdan o'tkazish
+        if username:
             db.register_user(telegram_id, fullname, username)
             await message.reply(
                 text=f"Assalomu aleykum, {fullname}, muvaffaqiyatli ro'yxatdan o'tdingiz ✅",
@@ -20,10 +30,9 @@ async def start(message: types.Message):
         else:
             db.register_user2(telegram_id, fullname)
             await message.reply(
-            text=f"Assalomu aleykum, {fullname}, muvaffaqiyatli ro'yxatdan o'tdingiz ✅",
-            reply_markup=generate_main_menu()
-        )
-
+                text=f"Assalomu aleykum, {fullname}, muvaffaqiyatli ro'yxatdan o'tdingiz ✅",
+                reply_markup=generate_main_menu()
+            )
     except Exception as e:
         await message.reply(
             text=f"Xush kelibsiz, {fullname} 🤝",
